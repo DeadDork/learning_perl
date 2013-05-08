3fae2b4fe796478ec6c97191e12d35c51235a121
DeadDork <nimrodomer@gmail.com>
2013-05-08 00:01:32 -0700

    Got to the end

--- perlintro/scratch_1.pl
+++ perlintro/scratch_1.pl
@@ -3,3 +3,34 @@
 use strict;
 use warnings;
 
+# Perl Variable Types {{{
+
+# Explore arrays
+#my @random = ( "Hello", "my", "honey,", "hello", "my", "darling,", "hello", "my", "clementine!" );
+#print "@random\n";
+#print "There are " . @random . ' elements in @random' . "\n";
+
+# Perl Variable Types }}}
+
+# Builtin Operators and Functions {{{
+
+# String multiplications (wtf?!)
+#my $string = "La ";
+#print $string x 70 . "\n";
+
+# Range operator
+#my $number = 7;
+#print $number .. ($number * 10);
+#print "\n";
+
+# Builtin Operators and Functions }}}
+
+# Writing Subroutines {{{
+
+#sub pow {
+#	my ( $base, $exponent ) = @_;
+#	return $base ** $exponent;
+#}
+
+#print pow 2, 8;
+#print "\n";
