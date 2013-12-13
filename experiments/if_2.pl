fc73e79ec2ba448158b22b8c0f0d25a26045e9f5
DeadDork <nimrodomer@gmail.com>
2013-12-12 20:29:16 -0800

    Init: explores 1 & 0 in a boolean context

---
+++ experiments/if_2.pl
@@ -0,0 +1,14 @@
+# Explores the use of 1 & 0 for truth values in a boolean context.
+
+# Conclusion: 1 & 0 can be used as truth values in a boolean context.
+
+use strict;
+use warnings;
+use 5.12.04;
+
+my $tv_1 = 0; # Truth Value 1
+my $tv_2 = 1; # Truth Value 2
+
+say "tv_1 is true" if $tv_1;
+
+say "tv_2 is true" if $tv_2;
