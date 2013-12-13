a25417fe4599c4033049354e30da38dd19337e7e
DeadDork <nimrodomer@gmail.com>
2013-12-12 20:07:55 -0800

    Init: tests variable expansion in source file

---
+++ experiments/variable_expansion.pl
@@ -0,0 +1,7 @@
+#!$HOME/.local/bin/perl # -bash: ./variable_expansion.pl: $HOME/.local/bin/perl: bad interpreter: No such file or directory
+
+use strict;
+use warnings;
+use 5.12.04;
+
+say "Hello";
